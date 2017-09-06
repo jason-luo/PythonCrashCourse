3da244a397a025e82f2ceb59443bcdd8c5ac7353
jason-luo <luo.jian@outlook.com>
2017-09-06 22:48:43 +0800

    设置屏幕背景色

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -11,11 +11,17 @@ def run_game():
     # 设置标题
     pygame.display.set_caption("Alien Invasion")
 
+    # 设置背景色
+    bg_color = (230, 230, 230)
+
     while True:
         # 获取并处理感兴趣的事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 填充屏幕背景色
+        screen.fill(bg_color)
 
         # 让绘制的屏幕可见
         pygame.display.flip()
