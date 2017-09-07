2ae1a13525636853affb17a4d0c52d65f4a836d4
jason-luo <luo.jian@outlook.com>
2017-09-08 00:22:22 +0800

    12.8 增加子弹发射

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -19,10 +20,16 @@ def run_game():
     # 创建飞船
     ship = Ship(ai_settings, screen)
 
+    bullets = Group()
+
     while True:
-        gf.check_event(ship)
+        gf.check_event(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+
+        gf.draw_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
 
 # 程序入口
 run_game()
--- AlienInvasion/game_functions.py
+++ AlienInvasion/game_functions.py
@@ -1,13 +1,16 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键按下"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -18,22 +21,37 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_event(ship):
+def check_event(ai_settings, screen, ship, bullets):
     """获取并处理感兴趣的事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
+def draw_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom < 0:
+            bullets.remove(bullet)
 
-def update_screen(ai_settings, screen, ship):
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def update_screen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像并切换到新屏幕"""
     # 填充屏幕背景色
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     # 显示飞船
     ship.blitme()
 
