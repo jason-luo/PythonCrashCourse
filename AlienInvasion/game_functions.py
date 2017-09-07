2f7bba24240e7055d7664a455d1607d26561cbe7
jason-luo <luo.jian@outlook.com>
2017-09-07 22:40:04 +0800

    12.6.6 重构check_events，把按下和松开extract两个函数

--- AlienInvasion/game_functions.py
+++ AlienInvasion/game_functions.py
@@ -2,22 +2,32 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """响应按键按下"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """响应按键放开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_event(ship):
     """获取并处理感兴趣的事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
