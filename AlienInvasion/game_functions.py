0b8956011bcfb71de9fab80a051a766dd92b4fe7
jason-luo <luo.jian@outlook.com>
2017-09-07 22:18:41 +0800

    12.6.3 飞船左右移动

--- AlienInvasion/game_functions.py
+++ AlienInvasion/game_functions.py
@@ -10,10 +10,14 @@ def check_event(ship):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
--- AlienInvasion/ship.py
+++ AlienInvasion/ship.py
@@ -16,10 +16,13 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        if (self.moving_right):
+        if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         # 指定位置绘制飞船
