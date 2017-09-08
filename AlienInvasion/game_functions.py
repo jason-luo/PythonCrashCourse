d601d5f11b8cff01513ef068e2bd11f2bb789670
jason-luo <luo.jian@outlook.com>
2017-09-08 22:56:08 +0800

    13.5.5 重构函数

--- AlienInvasion/game_functions.py
+++ AlienInvasion/game_functions.py
@@ -42,6 +42,10 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom < 0:
             bullets.remove(bullet)
 
+    check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
